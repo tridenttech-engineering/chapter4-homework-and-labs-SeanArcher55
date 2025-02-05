d984b4cbfc8adab72c0b34ddb796b1ae31e03c2f
SeanArcher55 <seanarcher@my.tridenttech.edu>
2025-02-05 14:44:29 -0500

    Update lab4-2.cpp
    Lab 4-2 first completion

--- lab4-2.cpp
+++ lab4-2.cpp
@@ -1,10 +1,19 @@
 //Lab4-2.cpp - displays a salesperson's commission
-//Philip Regalbuto
+//Sean Archer, 02-05-25
 #include <iostream>
 using namespace std;
 int main()
 {
+  double sales = 0.0;
+  double commission = 0.0;
 
+  const double RATE = .1;
+
+  cout<<"Sales amount: ";
+  cin>>sales;
+  commission = sales * RATE;
+
+  cout<<"Commission: $"<<commission<<endl;
 
   return 0;
 } //end of main function
