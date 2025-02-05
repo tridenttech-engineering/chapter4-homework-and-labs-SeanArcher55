564aa983a2e5e4fd923d50f608ffc47191bdcf95
SeanArcher55 <seanarcher@my.tridenttech.edu>
2025-02-05 14:56:52 -0500

    Update hw4-11.cpp

--- hw4-11.cpp
+++ hw4-11.cpp
@@ -1,11 +1,26 @@
 // Introductory11.cpp - displays ending balance
-// Created/revised by <your name> on <current date>
+// Created/revised by Sean Archer on 02-05-25
 
 #include <iostream>
 using namespace std;
 
 int main() {
-  
+  double totalDeposits = 0.0;
+  double totalWithdrawals = 0.0;
+  double startBalance = 0.0;
+  double endBalance = 0.0;
+
+  cout << "Enter starting balance: ";
+  cin >> startBalance;
+
+  cout << "Enter total deposits: ";
+  cin >> totalDeposits;
+
+  cout << "Enter total withdrawals: ";
+  cin >> totalWithdrawals;
+
+  endBalance = startBalance + totalDeposits - totalWithdrawals;
+  cout << "Ending balance: $" << endBalance << endl;
 
   return 0;
 } // end of main function
