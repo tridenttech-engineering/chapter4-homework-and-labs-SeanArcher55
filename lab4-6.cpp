def593226fccceef31586c19805ebcabaf232485
SeanArcher55 <seanarcher@my.tridenttech.edu>
2025-02-05 14:53:38 -0500

    Update lab4-6.cpp

--- lab4-6.cpp
+++ lab4-6.cpp
@@ -1,19 +1,19 @@
 //Lab4-6.cpp - displays the area of a triangle
-//Created/revised by <your name> on <current date>
+//Created/revised by Sean Archer on 02-05-25
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	//declare variables
-	int base = 11;
-	int height = 13;
-	double area = 0.0;
+  //declare variables
+  int base = 11;
+  int height = 13;
+  double area = 0.0;
 
-	//calculate and display area
-	area = base * height / 2;
-	cout << "Area: " << area << endl;
+  //calculate and display area
+  area = (double)base * height / 2;
+  cout << "Area: " << area << endl;
 
-	return 0;
+  return 0;
 }	//end of main function
