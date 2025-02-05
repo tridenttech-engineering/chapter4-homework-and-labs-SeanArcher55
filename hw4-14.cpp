13bb26a0b0b447323da6d8d4e897a9118f8c3681
SeanArcher55 <seanarcher@my.tridenttech.edu>
2025-02-05 15:00:30 -0500

    Update hw4-14.cpp

--- hw4-14.cpp
+++ hw4-14.cpp
@@ -5,7 +5,31 @@
 using namespace std;
 
 int main() {
-  
+  double small = 0.0;
+  double medium = 0.0;
+  double large = 0.0;
+  double family = 0.0;
+  double total = 0.0;
+
+  cout << "Small pizzas sold: ";
+  cin >> small;
+
+  cout << "Medium pizzas sold: ";
+  cin >> medium;
+
+  cout << "Large pizzas sold: ";
+  cin >> large;
+
+  cout << "Family pizzas sold: ";
+  cin >> family;
+
+  total = small + medium + large + family;
+  cout << endl;
+  cout << "Total pizzas sold: " << total << endl;
+  cout << "Small pizzas sold: " << small / total * 100 << "%" << endl;
+  cout << "Medium pizzas sold: " << medium / total * 100 << "%" << endl;
+  cout << "Large pizzas sold: " << large / total * 100 << "%" << endl;
+  cout << "Family pizzas sold: " << family / total * 100 << "%" << endl;
 
   return 0;
 } // end of main function
